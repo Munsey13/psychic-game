4399d08f9e8d5d5410ceb65a43eae632eb2223cc
MM13 <Fastsoccer13@gmail.com>
2019-10-05 17:45:44 -0400

    made variables for game

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+let wins = 0;
+let losses = 0;
+let guessesLeft = 9;
+let lettersGuessed = [];
+let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+
+ let computerPick = letters[]
+
