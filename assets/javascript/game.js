ec9ffcf9009a323bebe9868e9d43793e1f4247e1
MM13 <Fastsoccer13@gmail.com>
2019-10-05 20:57:12 -0400

    made JS file but can't get it to work properly and added id's to <p>'s in the html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,45 @@ let guessesLeft = 9;
 let lettersGuessed = [];
 let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
+let computerPick = letters[Math.floor(Math.random() *letters.length)];
 
- let computerPick = letters[]
+function restart() {
+    lettersGuessed = [];
+    guessesLeft = 9;
+    computerPick = letters[Math.floor(Math.random() *letters.length)];
+    document.getElementById("guesses-text").innerHTML = guessesLeft;
+}
 
+function win() {
+   wins++;
+   document.getElementById("win-text").innerHTML = winner;
+   restart(); 
+}
+
+function lost() {
+    losses++;
+    document.getElementById("lost-text").innerHTML = losses;
+    restart();
+}
+
+function guessed() {
+    let user = String.fromCharCode(event.keycode).toLowerCase();
+    lettersGuessed.push(user);
+    document.getElementById("guessed-text").innerHTML = lettersGuessed;
+}
+
+Document.onkeydown = function(){
+ let key = String.fromCharCode(event.keyCode).toLowerCase();
+ guessed()
+
+ if (key === computerPick) {
+     win();
+ }  else {
+    guessesLeft--;
+    document.getElementById("guesses-text").innerHTML = guessesLeft;
+ }
+
+ if (guessesLeft === 0) {
+     lost();
+ }
+}
